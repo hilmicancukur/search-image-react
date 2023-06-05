ea008a674c6ddf12d7b9624e7368be9a5f1ba4f9
hilmicancukur <hilmicancukur@gmail.com>
2023-06-05 14:47:17 +0300

    setValue when changed value with useState

--- src/components/SearchArea/SearchArea.js
+++ src/components/SearchArea/SearchArea.js
@@ -1,15 +1,19 @@
 import styles from "./SearchArea.module.scss";
-
+import { useState } from "react";
 
 function SearchArea({search}) {
+  const [value, setValue] = useState('');
   const handleSubmit = (e)=>{
     e.preventDefault();
     search('can');
   }
+  const changeValue = (e)=>{
+    setValue(e.target.value);
+  }
   return (
     <form action="GET" onSubmit={handleSubmit} className={styles.search__wrapper}>
       <label className="" htmlFor="lfor-search">Search images</label>
-      <input type="text" id="lfor-search" className="form-control"/>
+      <input type="text" id="lfor-search" className="form-control" value={value} onChange={changeValue}/>
     </form>
   );
 }
